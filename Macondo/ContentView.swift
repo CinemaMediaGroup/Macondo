0f99e2fa451fac838b794e76058fba0462dae20d
louisjshen <i@pixiv.world>
2020-07-12 17:21:29 +0800

    Initial Commit

---
+++ Macondo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Macondo
+//
+//  Created by Louis Joe Shen on 2020/7/12.
+//  Copyright © 2020 Louise Joe Shen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
