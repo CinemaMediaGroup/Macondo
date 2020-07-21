d093ca14969a640c939778d39a40b3b2292fb027
louisjshen <i@pixiv.world>
2020-07-21 17:53:04 +0800

    build: support for nav GUI

--- Macondo/ContentView.swift
+++ Macondo/ContentView.swift
@@ -9,12 +9,57 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     var body: some View {
-        Button(action: {
-            Sqlite.createTables()
-        }){
-            Text("Init")
+        VStack{
+            VStack{
+                Image("logo").resizable().frame(width: 128,height: 128)
+                Text("Welcome to Macondo")
+                    .font(.largeTitle)
+                    .fontWeight(.thin)
+                    
+                Text("Version 1.0")
+                    .font(.headline)
+                    .fontWeight(.thin)
+                    .foregroundColor(Color.gray)
+            }
+            .padding()
+            VStack(alignment: .leading){
+                Button(action: {
+                    
+                }) {
+                    HStack{
+                        Image("newPost").resizable().frame(width:32,height: 32)
+                        VStack(alignment: .leading){
+                            Text("Get started with a post")
+                                .fontWeight(.bold)
+                            Text("Create a post quickly and easily")
+                                .fontWeight(.thin)
+                                
+                        }
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+                Button(action: {
+                    
+                }){
+                    HStack{
+                        Image("newBook").resizable().frame(width:32,height: 32)
+                        Text("Create a new book-reading")
+                            .fontWeight(.bold)
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+                Button(action: {
+                    
+                }){
+                    HStack{
+                        Image("newLink").resizable().frame(width:32,height: 32)
+                        Text("Create a new link")
+                            .fontWeight(.bold)
+                    }
+                }
+                .buttonStyle(PlainButtonStyle())
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
