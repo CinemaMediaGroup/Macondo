0f08b7bd815ab2936932ee885a3c1f79c44bbaf0
Louise Shen <CinemaMediaGroup@iCloud.com>
2021-01-02 12:22:14 +0800

    refactor(postview.swift): separate ContentView into different views

--- Macondo/ContentView.swift
+++ Macondo/ContentView.swift
@@ -61,35 +61,6 @@ struct ContentView: View {
     }
 }
 
-struct PostView : View {
-    @EnvironmentObject var showView : ViewNavigation
-    var pds : [PostData] = [PostData]()
-    var lang : String
-    
-    init(language : String) {
-        pds = Sqlite.getPostList(language: language)
-        lang = language
-    }
-    
-    var body: some View {
-        NavigationView{
-            List(pds, id: \.self) { (pd)  in
-                NavigationLink(destination: EditPostView(cid: pd.getCid(),language: self.showView.lang)){
-                    VStack(alignment: .leading){
-                        Text(pd.getTitle()).font(.headline)
-                        Text(pd.getSummary()).fontWeight(.thin)
-                    }
-                }
-                .aspectRatio(contentMode: .fit)
-            }
-            //.listStyle(SidebarListStyle())
-            .listStyle(InsetListStyle())
-            
-            .frame(minWidth: 200, idealWidth: 350, maxWidth: 350, maxHeight: .infinity)
-        }
-    }
-}
-
 struct EditPostView : View{
     @EnvironmentObject var showView : ViewNavigation
     
@@ -124,7 +95,7 @@ struct EditPostView : View{
         VStack{
             HStack{
                 VStack{
-                    TextField(T.me(t: "Add title", language: self.showView.lang),text: $title)
+                    TextField(T.me(t: "Add title", language: self.showView.lang),text: $title)//.textFieldStyle(RoundedBorderTextFieldStyle())
                     TextEditor(text: $text)
                 }
                 VStack{
---
+++ Macondo/View/PostView.swift
@@ -0,0 +1,38 @@
+//
+//  PostView.swift
+//  Macondo
+//
+//  Created by Louis Shen on 2021/1/2.
+//
+
+import Foundation
+import SwiftUI
+
+struct PostView : View {
+    @EnvironmentObject var showView : ViewNavigation
+    var pds : [PostData] = [PostData]()
+    var lang : String
+    
+    init(language : String) {
+        pds = Sqlite.getPostList(language: language)
+        lang = language
+    }
+    
+    var body: some View {
+        NavigationView{
+            List(pds, id: \.self) { (pd)  in
+                NavigationLink(destination: EditPostView(cid: pd.getCid(),language: self.showView.lang)){
+                    VStack(alignment: .leading){
+                        Text(pd.getTitle()).font(.headline)
+                        Text(pd.getSummary()).fontWeight(.thin)
+                    }
+                }
+                .aspectRatio(contentMode: .fit)
+            }
+            //.listStyle(SidebarListStyle())
+            .listStyle(InsetListStyle())
+            
+            .frame(minWidth: 200, idealWidth: 350, maxWidth: 350, maxHeight: .infinity)
+        }
+    }
+}
